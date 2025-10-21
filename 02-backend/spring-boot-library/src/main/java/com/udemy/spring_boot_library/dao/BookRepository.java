5a78f12526b69df7ec77d3f0ac8061c275329c01
andrei <andrei_stoicu@yahoo.com>
2025-10-21 22:02:06 +0300

    back-end development

---
+++ 02-backend/spring-boot-library/src/main/java/com/udemy/spring_boot_library/dao/BookRepository.java
@@ -0,0 +1,8 @@
+package com.udemy.spring_boot_library.dao;
+
+import com.udemy.spring_boot_library.entity.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BookRepository extends JpaRepository<Book, Long> {
+
+}
